ca21865a985ac4892bf6153ec58ebb5ac046ffec
David Merchan <davidmerchan10@gmail.com>
2024-12-02 10:37:43 -0500

    Initial Commit

---
+++ NuevaAppTest/NuevaAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NuevaAppTestApp.swift
+//  NuevaAppTest
+//
+//  Created by David Merchan on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct NuevaAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
