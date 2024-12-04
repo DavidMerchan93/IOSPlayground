58fd9c0b516ab742a7ffcf7f97650ca1b82d7bb2
David Merchan <davidmerchan10@gmail.com>
2024-12-04 12:29:06 -0500

    Started playground proyect

--- NuevaAppTest/ContentView.swift
+++ NuevaAppTest/ContentView.swift
@@ -8,12 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let message = "Mi mensaje"
+    @State private var showAlert: Bool = false
+    
     var body: some View {
-        VStack {
+        VStack(
+            alignment: .leading,
+            spacing: 40
+        ) {
+            
+            
+            HStack {
+                Text("Hola")
+                Text("Que mas")
+                    .font(.title2)
+                    .bold()
+            }
+            
+            ZStack {
+                Color.red.edgesIgnoringSafeArea(.all)
+            }
+            
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Text("Hello!")
+                .font(.largeTitle)
+                .foregroundColor(Color.green)
+                .multilineTextAlignment(.trailing)
+                .padding([.leading, .bottom, .trailing], 20.0)
+            
+            Button(
+                action: {
+                    self.showAlert = true
+                }
+            ) {
+                Text(message)
+            }.alert(isPresented: $showAlert) {
+                Alert(title: Text("Titulo"), dismissButton:.default(Text("OK")))
+            }
         }
         .padding()
     }
