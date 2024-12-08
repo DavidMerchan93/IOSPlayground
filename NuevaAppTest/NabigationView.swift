e5a086d5a56770d4f74ead0db65fba49e124f5bb
David Merchan <davidmerchan10@gmail.com>
2024-12-08 11:21:20 -0500

    Created a navigation view

---
+++ NuevaAppTest/NabigationView.swift
@@ -0,0 +1,18 @@
+//
+//  NabigationView.swift
+//  NuevaAppTest
+//
+//  Created by David Merchan on 8/12/24.
+//
+
+import SwiftUI
+
+struct NabigationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NabigationView()
+}
---
+++ NuevaAppTest/SegundaVista.swift
@@ -0,0 +1,18 @@
+//
+//  SegundaVista.swift
+//  NuevaAppTest
+//
+//  Created by David Merchan on 8/12/24.
+//
+
+import SwiftUI
+
+struct SegundaVista: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SegundaVista()
+}
