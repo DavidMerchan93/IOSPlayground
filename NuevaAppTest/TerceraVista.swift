b87cfd336b77eb83324cc390c4f4a34193c982b3
David Merchan <davidmerchan10@gmail.com>
2024-12-20 14:30:34 -0500

    uso de los state en swiftUI

--- NuevaAppTest/TerceraVista.swift
+++ NuevaAppTest/TerceraVista.swift
@@ -12,13 +12,32 @@ struct TerceraVista: View {
     let colums = [GridItem(.adaptive(minimum: 60))]
     let rows: [GridItem] = Array(repeating: .init(.flexible()), count: 3)
     
+    @State private var showHeard = true
+    @State private var number = 0
+    
     var body: some View {
-        ScrollView {
+        VStack {
             LazyVGrid(columns: colums, spacing: 40) {
                 ForEach(lista) { item in
                     Text(item.emogi).font(.title)
                 }
+            }.background(.blue)
+            Button(action:  {
+                showHeard.toggle()
+                if showHeard {
+                    number -= 1
+                } else {
+                    number += 1
+                }
+            }) {
+                if showHeard {
+                    Image(systemName: "heart").foregroundColor(.red)
+                } else {
+                    Image(systemName: "heart.fill").foregroundColor(.red)
+                }
+                Text(String(number))
             }
+
         }
     }
 }
